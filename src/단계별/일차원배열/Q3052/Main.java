d63fa09d6baf8f8994c8cc100247121d7505566a
SEONGHYUN PARK <sparklep519@gmail.com>
2023-05-21 00:16:02 +0900

    feat:백준 Q3052

--- src/단계별/일차원배열/Q3052/Main.java
+++ src/단계별/일차원배열/Q3052/Main.java
@@ -3,25 +3,20 @@ package 단계별.일차원배열.Q3052;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int result = 0;
+        HashSet<Integer> hashSet = new HashSet<Integer>();
 
         for (int i = 0; i < 10; i++) {
-            st = new StringTokenizer(br.readLine());
-            int check = Integer.parseInt(st.nextToken());
-            if ((check % 42) == 0){
-                result++;
-            }
+            hashSet.add(Integer.parseInt(br.readLine()) % 42);
+
         }
-        sb.append(result);
+        sb.append(hashSet.size());
         br.close();
         System.out.println(sb);
     }
