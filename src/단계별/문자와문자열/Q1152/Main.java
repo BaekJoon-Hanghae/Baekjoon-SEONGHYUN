31caefe431325d2d8dddc33f3d38024a80c733aa
SEONGHYUN PARK <sparklep519@gmail.com>
2023-06-04 00:00:23 +0900

    feat:백준 Q1152

--- src/단계별/문자와문자열/Q1152/Main.java
+++ src/단계별/문자와문자열/Q1152/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st= new StringTokenizer(br.readLine());
