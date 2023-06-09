b0d0bd0b58256cdac5ffa3119fbfd1c75d3d5c4f
SEONGHYUN PARK <sparklep519@gmail.com>
2023-06-09 23:35:53 +0900

    feat:백준 Q2908

--- src/단계별/문자와문자열/Q2908/Main.java
+++ src/단계별/문자와문자열/Q2908/Main.java
@@ -10,12 +10,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
         int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-
-        System.out.println(a>b ? a:b);
+        sb.append(a>b ? a:b);
+        System.out.println(sb);
     }
 }
