f9cb1b644489a0c90ae80b0f8b4574b1983de7e9
SEONGHYUN PARK <sparklep519@gmail.com>
2023-06-17 23:49:03 +0900

    feat:백준 Q2444

---
+++ src/단계별/심화1/Q2444/Main.java
@@ -0,0 +1,22 @@
+package 단계별.심화1.Q2444;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<=i; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
