0989c7cdeed3adf1c87c3d29e2125610c43567f8
SEONGHYUN PARK <sparklep519@gmail.com>
2023-06-20 23:24:04 +0900

    feat:백준 Q1157

--- src/단계별/심화1/Q1157/Main.java
+++ src/단계별/심화1/Q1157/Main.java
@@ -5,30 +5,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int[] arr = new int[26];
         String s = br.readLine();
 
         for (int i = 0; i < s.length(); i++) {
             if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
                 arr[s.charAt(i) - 97]++;
-            }else {
+            } else {
                 arr[s.charAt(i) - 65]++;
             }
         }
         int max = -1;
         char ch = '?';
         for (int i = 0; i < 26; i++) {
-            if(arr[i] > max) {
+
+            if (arr[i] > max) {
                 max = arr[i];
                 ch = (char) (i + 65);
-            } else if (arr[i] == max) {
+            }
+            else if (arr[i] == max) {
                 ch = '?';
             }
         }
         sb.append(ch);
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
