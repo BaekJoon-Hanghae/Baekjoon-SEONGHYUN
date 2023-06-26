b1bb28b9caf84a4755d08eb9fcd37158467aedbf
SEONGHYUN PARK <sparklep519@gmail.com>
2023-06-26 23:32:02 +0900

    feat:백준 Q10798

--- src/단계별/이차원배열/Q10798/Main.java
+++ src/단계별/이차원배열/Q10798/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
         char[][] result = new char[5][15];
 
