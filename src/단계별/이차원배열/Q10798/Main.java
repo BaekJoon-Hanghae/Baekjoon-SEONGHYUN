b0bfb15340a64b85e499a68609cb6a8ccd112a91
SEONGHYUN PARK <sparklep519@gmail.com>
2023-06-29 22:59:50 +0900

    feat:백준 Q10798

--- src/단계별/이차원배열/Q10798/Main.java
+++ src/단계별/이차원배열/Q10798/Main.java
@@ -1,9 +1,7 @@
 package 단계별.이차원배열.Q10798;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
