03e8e56b1b9b8f88743871d395bba7b96416ccda
SEONGHYUN PARK <sparklep519@gmail.com>
2023-07-20 00:00:01 +0900

    feat:백준 Q2738

---
+++ src/단계별/일반수학1/Q2903/Main.java
@@ -0,0 +1,21 @@
+package 단계별.일반수학1.Q2903;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int width = 3;
+        int[] star = new int[16];
+        for(int i=1; i<star.length; i++) {	// 1 ~ 15
+            star[i] = width * width;
+            width = width + (width-1);
+        }
+
+        System.out.println(star[N]);
+        scan.close();
+    }
+}
