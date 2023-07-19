3c68215072463372328c63ee92628db4ddbee7d6
SEONGHYUN PARK <sparklep519@gmail.com>
2023-07-20 00:04:05 +0900

    feat:백준 Q2903

--- src/단계별/일반수학1/Q2903/Main.java
+++ src/단계별/일반수학1/Q2903/Main.java
@@ -1,21 +1,25 @@
 package 단계별.일반수학1.Q2903;
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
         int width = 3;
         int[] star = new int[16];
-        for(int i=1; i<star.length; i++) {	// 1 ~ 15
+        for(int i=1; i<star.length; i++) {
             star[i] = width * width;
             width = width + (width-1);
         }
+        sb.append(star[N]);
+        System.out.println(sb);
 
-        System.out.println(star[N]);
-        scan.close();
     }
 }
