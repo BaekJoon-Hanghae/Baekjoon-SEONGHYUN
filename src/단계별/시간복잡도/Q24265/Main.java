cdeb9faab83ec33771336bdbe611baeeffd2f911
SEONGHYUN PARK <sparklep519@gmail.com>
2023-10-30 23:42:34 +0900

    feat:백준 24265

--- src/단계별/시간복잡도/Q24265/Main.java
+++ src/단계별/시간복잡도/Q24265/Main.java
@@ -1,4 +1,4 @@
-package 단계별.시간복잡도.Q24263;
+package 단계별.시간복잡도.Q24265;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,10 +9,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+        long n = Long.parseLong(br.readLine());
 
-        sb.append(n).append("\n");
-        sb.append(1).append("\n");
+        sb.append((n*(n-1))/2).append("\n");
+        sb.append(2);
 
         System.out.print(sb);
         br.close();
