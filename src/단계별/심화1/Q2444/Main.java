384c86b473ddfe0f734b894c9000b49e6f3cfbba
SEONGHYUN PARK <sparklep519@gmail.com>
2024-03-17 23:32:16 +0900

    feat:백준 2444

--- src/단계별/심화1/Q2444/Main.java
+++ src/단계별/심화1/Q2444/Main.java
@@ -9,14 +9,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<=i; j++){
-                sb.append("*");
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            if (i <= n) {
+                for (int j = 1; j <= n - i; j++) {
+                    sb.append(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    sb.append("*");
+                }
+            } else {
+                for (int j = 1; j <= i % n; j++) {
+                    sb.append(" ");
+                }
+                for (int j = 1; j <= (2 * n - 1) - 2 * (i % n); j++) {
+                    sb.append("*");
+                }
             }
             sb.append("\n");
         }
-        System.out.print(sb);
+        br.close();
+
     }
 }
